6c2d31cb148fb77d7e6a3fca71d17a1a4dac6e02
crisandryj <crisandryj@gmail.com>
2023-07-11 20:23:38 -0400

    Feat: Add function using myName

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-import _ from 'lodash';
+import myName from './myName';
+
 
 function component() {
   const element = document.createElement('div');
 
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
+  // use your function!
+  element.textContent = myName('Cody');
   return element;
 }
+
 
 document.body.appendChild(component());
