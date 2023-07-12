52d7e7951801366ac39b484749e40c6e336b2bf8
crisandryj <crisandryj@gmail.com>
2023-07-11 20:26:20 -0400

    run build

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,1 @@
-(()=>{"use strict";document.body.appendChild(function(){const e=document.createElement("div");return e.innerHTML=_.join(["Hello","webpack"]," "),e}())})();
+(()=>{"use strict";document.body.appendChild(function(){const t=document.createElement("div");return t.textContent="Hi! My name is "+"Cody",t}())})();
