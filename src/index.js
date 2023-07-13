b7d4372ae35fbb88da15c3eae274138d3869b532
crisandryj <crisandryj@gmail.com>
2023-07-13 12:25:39 -0400

    Add red font

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import myName from './myName';
+import './style.css';
 
 
 function component() {
   const element = document.createElement('div');
+  element.classList.add("hello")
 
   // use your function!
   element.textContent = myName('Cody');
